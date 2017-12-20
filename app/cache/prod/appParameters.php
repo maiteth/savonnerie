f70ab27fde694e4508ce6e315a399018fff25142
Maite THOMIAS <maite.thomias@gmail.com>
2017-12-20 10:21:28 +0100

    init

---
+++ app/cache/prod/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'savonnerie',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'sav_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'uLHXVJhusjxOJyGnn03cwMyJUipyHuYrFvGH2CbTa6mF61fF8svWWzwG',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-12-20',
+    'locale' => 'en-US',
+    'cookie_key' => 'SOYfrOfkpPp53bvDkEOo2aEdA3QhfxV29BgwJDYZ60DsOg5tx0Z3EjTF',
+    'cookie_iv' => 'AQLEBGx7',
+    'new_cookie_key' => 'def0000084335f6f5a7d6b0bc46a8a94e302b12a4f46654ec74898aec945025e3686d655a4f96ee54c4093f62b8dfa3ac93f7f62078cb0f6ab5f2ecd1de2ed84ac878389',
+  ),
+);
